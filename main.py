bcee88b1b49ded22898d488fbd25eaf59cdb4f0e
zhao <1934109821@qq.com>
2021-09-21 17:32:50 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import cv2
+import numpy
 from PyQt5 import QtGui
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtGui import QImage, QPixmap
@@ -31,11 +32,13 @@ class PretreatmentWindow(QWidget, Ui_Form):
     def sliderChange(self):
         value = self.slider.value()
         self.spinBox.setValue(value)
-        # self.signal.emit(value)
 
     def valueConfirm(self):
         self.signal.emit('ok')
         self.close()
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        self.signal.emit('close')
 
 
 # 主窗口类
@@ -53,6 +56,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.__srcImageRGB = None
         # 结果图片数据矩阵
         self.__outImageRGB = None
+        # 暂存未确认保存的修改数据矩阵
+        self.__tempImageRGB = None
 
         # 绑定窗口事件的响应函数
         self.openFileAction.triggered.connect(self.openFileAndShowImage)
@@ -66,6 +71,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.binaryAction.triggered.connect(self.toBinaryImage)
         self.reverseAction.triggered.connect(self.reverseImage)
         self.lightAction.triggered.connect(self.openLightWindow)
+        self.contrastAction.triggered.connect(self.openContrastWindow)
+        self.sharpAction.triggered.connect(self.openSharpWindow)
 
     # 打开文件并在主窗口中显示打开的图像
     def openFileAndShowImage(self):
@@ -75,6 +82,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             __bgrImg = cv2.imread(self.__fileName)
             self.__srcImageRGB = cv2.cvtColor(__bgrImg, cv2.COLOR_BGR2RGB)
             self.__outImageRGB = self.__srcImageRGB.copy()
+            self.__tempImageRGB = self.__srcImageRGB.copy()
             self.__drawImage(self.srcImageView, self.__srcImageRGB)
             self.__drawImage(self.outImageView, self.__srcImageRGB)
 
@@ -91,7 +99,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             location.setScene(__scene)
         # 灰度图、二值图
         else:
-            __width, __height = img.shape
+            __height, __width = img.shape
             __qImg = QImage(img, __width, __height, __width, QImage.Format_Indexed8)
             __qPixmap = QPixmap.fromImage(__qImg)
             __scene = QGraphicsScene()
@@ -156,15 +164,111 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     # 打开图像预处理的亮度调节子窗口
     def openLightWindow(self):
-        self.__pretreatmentWindow = PretreatmentWindow()
-        self.__pretreatmentWindow.setWindowTitle('亮度')
-        self.__pretreatmentWindow.propertyLabel.setText('亮度')
-        self.__pretreatmentWindow.show()
-        self.__pretreatmentWindow.signal.connect(self.changeLight)
+        if self.__fileName:
+            self.__pretreatmentWindow = PretreatmentWindow()
+            self.__pretreatmentWindow.setWindowTitle('亮度')
+            self.__pretreatmentWindow.propertyLabel.setText('亮度')
+            self.__pretreatmentWindow.show()
+            self.__pretreatmentWindow.signal.connect(self.changeLight)
 
+    # 打开图像预处理的对比度调节子窗口
+    def openContrastWindow(self):
+        if self.__fileName:
+            self.__pretreatmentWindow = PretreatmentWindow()
+            self.__pretreatmentWindow.setWindowTitle('对比度')
+            self.__pretreatmentWindow.propertyLabel.setText('对比度')
+            self.__pretreatmentWindow.show()
+            self.__pretreatmentWindow.signal.connect(self.changeContrast)
+
+    # 打开图像预处理的锐度调节子窗口
+    def openSharpWindow(self):
+        if self.__fileName:
+            self.__pretreatmentWindow = PretreatmentWindow()
+            self.__pretreatmentWindow.setWindowTitle('锐度')
+            self.__pretreatmentWindow.propertyLabel.setText('锐度')
+            self.__pretreatmentWindow.show()
+            self.__pretreatmentWindow.signal.connect(self.changeSharp)
+
+    # 改变亮度或对比度
+    # g(i,j)=αf(i,j)+(1-α)black+β，α用来调节对比度, β用来调节亮度
+    def lightAndContrast(self, img, alpha, beta):
+        if len(img.shape) < 3:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        rows, cols, channels = img.shape
+        # 新建全零(黑色)图片数组
+        blank = numpy.zeros([rows, cols, channels], img.dtype)
+        # 计算两个图像阵列的加权和
+        img = cv2.addWeighted(img, alpha, blank, 1 - alpha, beta)
+        # 显示修改数据
+        self.__drawImage(self.outImageView, img)
+        return img
+
+    # 修改亮度
     def changeLight(self, val):
-        print(val)
-#         TODO
+        # 拷贝后修改副本
+        __img = self.__outImageRGB.copy()
+        value = str(val)
+
+        # 确认修改
+        if value == 'ok':
+            # 将暂存的修改保存为结果
+            self.__outImageRGB = self.__tempImageRGB.copy()
+        # 修改完成（确认已经做的修改或取消了修改）
+        elif value == 'close':
+            # 重绘修改预览
+            self.__drawImage(self.outImageView, self.__outImageRGB)
+        # 暂时修改
+        else:
+            beta = int(value) * (255 / 100)
+            # 暂存本次修改
+            self.__tempImageRGB = self.lightAndContrast(__img, 1, beta)
+
+    # 修改对比度
+    def changeContrast(self, val):
+        # 拷贝后修改副本
+        __img = self.__outImageRGB.copy()
+        value = str(val)
+
+        # 确认修改
+        if value == 'ok':
+            # 将暂存的修改保存为结果
+            self.__outImageRGB = self.__tempImageRGB.copy()
+        # 修改完成（确认已经做的修改或取消了修改）
+        elif value == 'close':
+            # 重绘修改预览
+            self.__drawImage(self.outImageView, self.__outImageRGB)
+        # 暂时修改
+        else:
+            k = int(value)
+            if k != -100:
+                alpha = (k + 100) / 100
+            else:
+                alpha = 0.01
+            # 暂存本次修改
+            self.__tempImageRGB = self.lightAndContrast(__img, alpha, 0)
+
+    def changeSharp(self, val):
+        # 拷贝后修改副本
+        __img = self.__outImageRGB.copy()
+        value = str(val)
+
+        # 确认修改
+        if value == 'ok':
+            # 将暂存的修改保存为结果
+            self.__outImageRGB = self.__tempImageRGB.copy()
+        # 修改完成（确认已经做的修改或取消了修改）
+        elif value == 'close':
+            # 重绘修改预览
+            self.__drawImage(self.outImageView, self.__outImageRGB)
+        # 暂时修改
+        else:
+            k = int(value) * 0.01
+            # 卷积核
+            kernel = numpy.array([[0, -1, 0], [-1, 5 + k, -1], [0, -1, 0]])
+            # 通过卷积实现锐化,暂存修改数据
+            self.__tempImageRGB = cv2.filter2D(__img, -1, kernel)
+            # 显示修改数据
+            self.__drawImage(self.outImageView, self.__tempImageRGB)
 
 
 if __name__ == '__main__':
